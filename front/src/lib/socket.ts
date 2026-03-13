1414b698df07b5ce29be1b6ecde7e24b525abf21
Baptiste Louyot <baptiste.louyot@gmail.com>
2026-03-13 19:36:41 +0100

    Back seul

--- front/src/lib/socket.ts
+++
@@ -1,3 +0,0 @@
-import { io } from 'socket.io-client';
-export const socket = io(window.location.origin, { autoConnect: false, reconnection: true, reconnectionAttempts: 3, reconnectionDelay: 3000, reconnectionDelayMax: 10000, timeout: 10000, transports: ['websocket'] });
-export function ensureSocketConnected() { if (!socket.connected && !socket.active) socket.connect(); }
